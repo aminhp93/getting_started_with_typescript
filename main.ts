22e13a75d1bb6a256e2e17920a6d28ebf8bcdac3
aminhp93 <minhpn.org.ec@gmail.com>
2017-06-22 16:27:50 -0700

    04 - Run & Compile with Webpack

--- main.ts
+++ main.ts
@@ -1,40 +1,15 @@
-class SweetSweetBasil{
-	color:string;
-	name:string;
-	// constructor(name:string, startColor:string){
-	// 	console.log("Hello " + name)
-	// 	this.color = startColor
-	// }
-	constructor(){
-		
-	}
+import {MustHaveCoffee} from "./src/coffee/getcoffee"
 
-	getColor(){
-		console.log(this.color)
+function f(input:boolean){
+	let a = 100
+	if (input){
+		let b = a + 123
+		return b
 	}
+	return a;
 }
 
-let basil = new SweetSweetBasil("World", "green")
-basil.getColor()
+console.log(f(true))
+console.log(f(false))
 
-console.log(basil.color);
-
-class Basil extends SweetSweetBasil{
-	constructor(name:string, startColor:string){
-		// super(name, startColor)
-		super()
-		this.name = name
-		this.color = startColor
-	}
-
-	setNewColor(newColor:string){
-		this.color = newColor
-		super.getColor()
-	}
-}
-
-let basil3 = new Basil("basil", "bright green")
-
-basil3.getColor()
-basil3.setNewColor("gray")
-basil3.getColor()
+let coffee = new MustHaveCoffee()
---
+++ src/coffee/getcoffee.ts
@@ -0,0 +1,5 @@
+export class MustHaveCoffee{
+	constructor(){
+		console.log("Make a bulletproff")
+	}
+}
