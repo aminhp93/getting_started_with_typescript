f1217a2ccaee662edfffe82db56418f88668ae77
aminhp93 <minhpn.org.ec@gmail.com>
2017-06-22 15:39:08 -0700

    03 - Typescript - Classes - Part 2

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,39 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var SweetSweetBasil = (function () {
-    function SweetSweetBasil(name) {
-        console.log("Hello " + name);
+    function SweetSweetBasil() {
     }
-    SweetSweetBasil.prototype.color = function () {
-        console.log("Green");
+    SweetSweetBasil.prototype.getColor = function () {
+        console.log(this.color);
     };
     return SweetSweetBasil;
 }());
-var basil = new SweetSweetBasil("World");
-basil.color();
+var basil = new SweetSweetBasil("World", "green");
+basil.getColor();
+console.log(basil.color);
+var Basil = (function (_super) {
+    __extends(Basil, _super);
+    function Basil(name, startColor) {
+        var _this = _super.call(this) || this;
+        _this.name = name;
+        _this.color = startColor;
+        return _this;
+    }
+    Basil.prototype.setNewColor = function (newColor) {
+        this.color = newColor;
+        _super.prototype.getColor.call(this);
+    };
+    return Basil;
+}(SweetSweetBasil));
+var basil3 = new Basil("basil", "bright green");
+basil3.getColor();
+basil3.setNewColor("gray");
+basil3.getColor();
--- main.ts
+++ main.ts
@@ -1,12 +1,40 @@
 class SweetSweetBasil{
-	constructor(name:string){
-		console.log("Hello " + name)
+	color:string;
+	name:string;
+	// constructor(name:string, startColor:string){
+	// 	console.log("Hello " + name)
+	// 	this.color = startColor
+	// }
+	constructor(){
+		
 	}
 
-	color(){
-		console.log("Green")
+	getColor(){
+		console.log(this.color)
 	}
 }
 
-let basil = new SweetSweetBasil("World")
-basil.color()
+let basil = new SweetSweetBasil("World", "green")
+basil.getColor()
+
+console.log(basil.color);
+
+class Basil extends SweetSweetBasil{
+	constructor(name:string, startColor:string){
+		// super(name, startColor)
+		super()
+		this.name = name
+		this.color = startColor
+	}
+
+	setNewColor(newColor:string){
+		this.color = newColor
+		super.getColor()
+	}
+}
+
+let basil3 = new Basil("basil", "bright green")
+
+basil3.getColor()
+basil3.setNewColor("gray")
+basil3.getColor()
